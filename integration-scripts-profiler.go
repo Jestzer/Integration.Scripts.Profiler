ad2e9ecf91ad9ef7d064bb8ecad696dc1d829dc5
Jestzer <james@buonopane.email>
2024-01-20 20:56:05 -0500

    Basic fake functionality.

--- integration-scripts-profiler.go
+++ integration-scripts-profiler.go
@@ -10,6 +10,7 @@ import (
 	"os/signal"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"syscall"
 
@@ -25,11 +26,18 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 
 	// Goodies.
+	var input string
 	var defaultTMP string
 	var schedulerSelected string
 	var organizationSelected string
 	var clusterCount int
 	var clusterName string
+	var submissionType string
+	var numberOfWorkers int
+	var hasSharedFileSystem bool
+	var clusterMatlabRoot string
+	var clusterHostname string
+	var remoteJobStorageLocation string
 
 	// # Add some code that'll load any preferences for the program.
 	// # Add some code that'll allow arrow keys to be used when prompted for user input.
@@ -68,7 +76,7 @@ func main() {
 		organizationSelected = strings.TrimSpace(organizationSelected)
 
 		if organizationSelected == "" {
-			fmt.Print("Invalid entry. ")
+			fmt.Print(redText("Invalid entry. "))
 			continue
 		} else {
 			break
@@ -87,7 +95,7 @@ func main() {
 		clusterName = strings.TrimSpace(clusterName)
 
 		if clusterName == "" {
-			fmt.Print("Invalid entry. ")
+			fmt.Print(redText("Invalid entry. "))
 			continue
 		} else {
 			break
@@ -105,6 +113,109 @@ func main() {
 		fmt.Print(defaultTMP)
 	}
 
+	for {
+		fmt.Print("Does the cluster allow submissions from remote, cluster, or both places? Entering nothing will select both.\n")
+		submissionType, _ = reader.ReadString('\n')
+		submissionType = strings.TrimSpace(strings.ToLower(submissionType))
+
+		if submissionType == "" {
+			submissionType = "both"
+			break
+		} else if submissionType == "cluster" || submissionType == "remote" || submissionType == "both" {
+			break
+		} else {
+			fmt.Print(redText("Invalid entry. "))
+			continue
+		}
+	}
+
+	for {
+		fmt.Print("Enter the number of workers available on the cluster's license. Entering nothing will select 100,000.\n")
+		input, _ = reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if input == "" {
+			numberOfWorkers = 100000
+			break
+			fmt.Print(numberOfWorkers) // Again, Go, shut up.
+		}
+
+		// Don't accept anything other than numbers.
+		if _, err := strconv.Atoi(input); err == nil {
+			numberOfWorkers, _ = strconv.Atoi(input)
+			break
+		} else {
+			fmt.Print(redText("Invalid entry. "))
+			continue
+		}
+	}
+	if submissionType == "remote" || submissionType == "both" {
+
+		for {
+			fmt.Print("Does the client have a shared filesystem with the cluster? (y/n)\n")
+			input, _ = reader.ReadString('\n')
+			input = strings.TrimSpace(strings.ToLower(input))
+
+			if input == "y" || input == "yes" {
+				hasSharedFileSystem = true
+				break
+				fmt.Print(hasSharedFileSystem) // Again, shut up, Go. It'll be used at some point, I promise.
+			} else if input == "n" || input == "no" {
+				hasSharedFileSystem = false
+				break
+			} else {
+				fmt.Print(redText("Invalid entry. "))
+				continue
+			}
+		}
+
+		for {
+			fmt.Print("What is the full filepath of MATLAB on the cluster? (ex: /usr/local/MATLAB/R2023b)\n")
+			clusterMatlabRoot, _ = reader.ReadString('\n')
+			clusterMatlabRoot = strings.TrimSpace(clusterMatlabRoot)
+
+			if strings.Contains(clusterMatlabRoot, "/") || strings.Contains(clusterMatlabRoot, "\\") {
+				break
+			} else {
+				fmt.Print(redText("Invalid filepath. "))
+				continue
+			}
+		}
+
+		for {
+			fmt.Print("What is the hostname, FQDN, or IP address used to SSH to the cluster?\n")
+			clusterHostname, _ = reader.ReadString('\n')
+			clusterHostname = strings.TrimSpace(clusterHostname)
+
+			if clusterHostname == "" {
+				fmt.Print(redText("Invalid entry. "))
+				continue
+			} else {
+				break
+			}
+		}
+
+		for {
+			fmt.Print("Where will remote job storage location be on the cluster? Entering nothing will select /home/$User/.matlab/generic_cluster_jobs/$ClusterName/$Host\n")
+			remoteJobStorageLocation, _ = reader.ReadString('\n')
+			remoteJobStorageLocation = strings.TrimSpace(remoteJobStorageLocation)
+
+			if strings.Contains(remoteJobStorageLocation, "/") || strings.Contains(clusterMatlabRoot, "\\") {
+				break
+			} else if remoteJobStorageLocation == "" {
+				remoteJobStorageLocation = "/home/$USER/.matlab/generic_cluster_jobs/" + clusterName + "/$HOST"
+				break
+			} else {
+				fmt.Print(redText("Invalid filepath. "))
+				continue
+			}
+		}
+
+	}
+	fmt.Print("Creating integration scripts...\n")
+	fmt.Print("Finished!\n")
+	fmt.Print("Submitting to GitLab...\n")
+	fmt.Print("Finished!\n")
 }
 
 // Function to download a file from a given URL and save it to the specified path.
